eaac2c9f06a6a7ed775cbce6549396ecf84394f0
Brandon <brandonkemboi87@gmail.com>
2025-08-05 11:41:10 -0500

    Initial Commit

---
+++ TennisRating/TennisRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisRatingApp.swift
+//  TennisRating
+//
+//  Created by Brandon Kemboi on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TennisRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TennisRatingTests/TennisRatingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TennisRatingTests.swift
+//  TennisRatingTests
+//
+//  Created by Brandon Kemboi on 8/5/25.
+//
+
+import Testing
+@testable import TennisRating
+
+struct TennisRatingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
